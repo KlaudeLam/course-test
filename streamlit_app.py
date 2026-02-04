a0752388c4e42d59b6ce8201cae4e0e465232c65
Ky Lam <101573199+KlaudeLam@users.noreply.github.com>
2026-02-04 17:46:38 +0000

    First class

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,32 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Welcome to the DS Class")
+st.subheader("Welcome to the DS Class")
+st.write("Hello world")
+st.caption("This is a caption")
+
+# Include beyond text
+st.code("x=6767")
+st.latex(r'''a+a = e^-1''')
+
+st.image("image.png", )
+
+# Interactive
+st.checkbox("yes")
+st.button("click")
+st.radio("choose",["a","b"])
+st.multiselect("Pick",["a","b"])
+st.slider("sliede",0,100)
+
+# Advanced input
+st.number_input("choose a no.",0,10)
+st.text_input("email")
+st.date_input("bday")
+
+st.sidebar.file_uploader("Gib cat meme")
+
+# Display CSV
+df = pd.read_csv("bnpl_dataset.csv")
+st.dataframe(df.head(5))
